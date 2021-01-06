9f6c7f18866664ddcdebaa2c4cbfc633216104f8
John Grana <jgrana@rochester.rr.com>
2021-01-06 14:32:24 -0500

    Update migratetoYaz.sh
    Changed filename from DHCP_Clients to DHCP_clients

--- migratetoYaz.sh
+++ migratetoYaz.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Generate /jffs/addons/YazDHCP.d/DHCP_Clients compatible file if a /jffs/configs/dnsmasq.conf.add file exists.
-# Once checked, can be optionaly be copied to  /jffs/addons/YazDHCP.d/DHCP_Clients
+# Once checked, can be optionaly be copied to  /jffs/addons/YazDHCP.d/DHCP_clients
 if [ -f /jffs/configs/dnsmasq.conf.add ]; then
 	if ! [ -d /jffs/addons/YazDHCP.d ]; then
 		echo "It doesn't appear that you have installed YazDHCP"
@@ -10,11 +10,11 @@ if [ -f /jffs/configs/dnsmasq.conf.add ]; then
 	echo "dnsmasq.conf.add found, generating YazDHCP compatible file"
 	cat /jffs/configs/dnsmasq.conf.add | grep '^dhcp-host=' | awk -F"," '{ printf substr($1,11)","$2 "," $3 ",";if ($4) printf $4 ; printf "\n" }' > cvtYazDHCP
 	more cvtYaxDHCP
-	echo -n "If this file looks good, enter Y copy to /jffs/addons/YazDHCP.d/DHCP_Clients "
+	echo -n "If this file looks good, enter Y copy to /jffs/addons/YazDHCP.d/DHCP_clients "
 	read -r "confirm"
 	case "$confirm" in
 		y|Y)
-			cp ./cvtYazDHCP /jffs/addons/YazDHCP.d/DHCP_Clients
+			cp ./cvtYazDHCP /jffs/addons/YazDHCP.d/DHCP_clients
 			rm ./cvtYazDHCP
 			echo "Copied"
 			echo "Your dnsmasq.conf.add file in /jffs/configs should be disabled/backed up"
@@ -24,6 +24,7 @@ if [ -f /jffs/configs/dnsmasq.conf.add ]; then
                 		y|Y)
                         		mv /jffs/configs/dnsmasq.conf.add /jffs/configs/dnsmasq.conf.bak
 					echo "renamed..."
+					echo
                         		break
                 		;;
                 		*)
